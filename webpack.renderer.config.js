796d9c135e5a97135292330c4aa723daa51a1d4e
Kennys Ng (ESD) - EXT <ext-esdkeng@hktdc.org>
2022-08-02 12:51:12 +0800

    feat: handle assets

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,21 +1,35 @@
+const { readdirSync } = require('fs')
+const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
-rules.push({
-  test: /\.css$/,
-  use: ['style-loader', 'css-loader'],
-});
-
-rules.push({
-  test: /\.s[ac]ss$/i,
-  use: ["style-loader", "css-loader", "sass-loader"],
-},)
+const folders = readdirSync(resolve(__dirname, 'assets'))
+  .filter(folder => readdirSync(resolve(__dirname, 'assets', folder)).length)
 
 module.exports = {
   module: {
-    rules,
+    rules: [
+      ...rules,
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      }
+    ],
   },
-  plugins: plugins,
+  plugins: [
+    ...plugins,
+    new CopyWebpackPlugin({
+      patterns: folders.map(asset => ({
+        from: resolve(__dirname, 'assets', asset),
+        to: resolve(__dirname, '.webpack/assets', asset)
+      }))
+    })
+  ],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
   },
